2460b4bf84670945260bd87d152f44f9aa895fd9
Davi Gabriel Zambiazi Taborda <davi.taborda07@gmail.com>
2024-08-30 21:43:51 -0300

    Resolução dos exercícios da Target

---
+++ TesteTarget/Soma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Soma
+{
+    static void Main(string[] args)
+    {
+        int indice = 12;
+        int soma = 0;
+        int K = 1;
+
+        while (K < indice)
+        {
+            K += K;
+            soma = soma + K;
+        }
+
+        Console.WriteLine("O valor final da soma é:" + soma);
+    }
+}
+
---
+++ TesteTarget/StringA.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class StringA { 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma frase");
+        String input = Console.ReadLine();
+
+        int quantidade = ContarOcorrencias(input);
+
+        if (quantidade > 0)
+        {
+            Console.WriteLine("A letra 'A' aparece" + quantidade + " vezes na frase");
+        }
+        else 
+        {
+            Console.WriteLine("A letra 'A' não aparece na String");
+        }
+    }
+
+
+    //Função que conta as ocorrências de A
+    static int ContarOcorrencias(string texto)
+    {
+        int contador = 0;
+
+        foreach (char c in texto)
+        {
+            if (c == 'a' || c == 'A') 
+            {
+                contador++;
+            }
+        }
+
+        return contador;
+
+    }
+}
+
